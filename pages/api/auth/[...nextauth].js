5496850c48022017066288d6bcc29719dae9be31
bayyubenjamin <69312778+bayyubenjamin@users.noreply.github.com>
2025-05-29 16:26:31 +0700

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,9 +5,9 @@ import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+ GoogleProvider({
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // Aktifkan jika sudah siap:
     // TelegramProvider({
